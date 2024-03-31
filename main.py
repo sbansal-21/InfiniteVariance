d186abbeb9290e14effd1d5bca5ebbbd6d171207
sbansal-21 <sbansal.hf@gmail.com>
2024-03-31 17:36:52 +0100

    Added some code

--- main.py
+++ main.py
@@ -1,4 +1,23 @@
 import numpy as np
-np.sqrt(2)
+import matplotlib.pyplot as plt
+import os
+import datetime
+import random
+import sys
+import yfinance as yf
+
+os.getcwd()
+
+today = datetime.datetime.now().today()
+numbers_of_years = 5
+start_date = today - datetime.timedelta(numbers_of_years*365)
+
+start_date = start_date.strftime("%Y-%m-%d")
+end_date = today.strftime("%Y-%m-%d")
+tickers = ["AAPL"]
+
+stock_data = yf.Ticker(tickers[0])
+historical_data = stock_data.history(start=start_date, end=end_date)
+historical_data.head()
 
 
